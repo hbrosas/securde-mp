9aeb518da339303b7943340edbc0e5b4c7c5379f
Randolph Yu <Randolph Yu>
2017-07-06 04:40:58 +0800

    Created edit/delete book page

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,3 +1,21 @@
+$(document).ready(function() {
+    $('#booksTable').DataTable();
+} );
 $('#addButton').click(function(){
-  swal("Book Title", "has been added to the catalog", "success")
+  swal("Book Title", "has been added to the catalog.", "success")
+})
+
+$('#deleteBook').click(function(){
+  swal({
+  title: "Are you sure?",
+  text: "You will not be able to recover this book.",
+  type: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#DD6B55",
+  confirmButtonText: "Delete",
+  closeOnConfirm: false
+},
+function(){
+  swal("Deleted", "Magic 101 has been deleted.", "success");
+});
 })
